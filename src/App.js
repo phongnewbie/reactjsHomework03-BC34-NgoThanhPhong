ccf093f92b0aa25607f6ae90ab0cc614dac7a0a0
NgoThanhPhong <phongngo290902@gmail.com>
2022-10-26 09:00:36 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 
-import TrangChuSanPham from './Component/TrangChuSanPham.JSX';
+import TrangChuSanPham from './Component/TrangChuSanPham.jsx';
 
 function App() {
   return (
     <div className="App">
-       
+
         <TrangChuSanPham/>
         
     </div>
